b1ee0ff9e65fc5e1610f01e8865e33bc050863ce
Zak Fallows <zakf@mit.edu>
2016-05-26 18:56:06 -0400

    Custom template tags: I have implemented the {% wiki %} template tag, it works well.

--- source/dehr_template_tags.py
+++ source/dehr_template_tags.py
@@ -33,3 +33,32 @@ def indent():
 @register.simple_tag
 def endindent():
     return mark_safe('</div> <!-- div.indent -->')
+
+
+WIKI_URL = ("http://en.wikipedia.org/w/index.php?"
+            "search=%s&title=Special%%3ASearch")
+
+@register.inclusion_tag('old_wiki_tag.html')
+def old_wiki(target, display=None):
+    """DEPRECATED
+    
+    This tag should NOT be an inclusion_tag() because it is too simple. When made as an inclusion tag, it either inserts a newline at the end OR you need to OMIT the final newline in the old_wiki_tag.html file, which is bad code style. I hate to make a file with no newline at the end.
+    
+    """
+    
+    if not display:
+        display = target
+    template_vars = {'display': display, 'url': WIKI_URL % target}
+    return template_vars
+
+@register.simple_tag
+def wiki(target, display=None):
+    """Include a link to Wikipedia
+    
+    """
+    
+    if not display:
+        display = target
+    url_str = WIKI_URL % target
+    html_str = '<a href="%s">%s</a>' % (url_str, display)
+    return mark_safe(html_str)
